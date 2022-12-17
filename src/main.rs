76bb3113fc838201b56ccb5a3033b30b602fc80b
rip <valentin.michaud73@gmail.com>
2022-12-17 14:55:40 +0100

    faster random bytes and dome comments added

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,62 @@
-use std::{fs::File, thread::spawn, collections::HashMap, env};
-use rand::Rng;
-use libsecp256k1::{PublicKey, SecretKey};
-use sha3::{Digest, Keccak256};
-use log;
-use std::sync::mpsc::{channel, Sender};
-use simplelog::*;
-use chrono;
+use std::time::Instant; // used for measuring elapsed time
+use std::{
+    fs::File, // used for creating a log file
+    thread::spawn, // used for spawning threads
+    collections::HashMap, // used for storing command line arguments
+    env,
+};
+use rand_chacha::ChaChaRng; // used for generating random numbers
+use libsecp256k1::{PublicKey, SecretKey}; // used for generating secret and public keys
+use sha3::{Digest, Keccak256}; // used for generating hashes using the Keccak-256 algorithm
+use log; // used for logging messages
+use std::sync::mpsc::{channel, Sender}; // used for sending messages between threads
+use simplelog::*; // used for setting up logging to both a file and the terminal
+use chrono; // used for getting the current date and time
+use rand_chacha::rand_core::SeedableRng;
+use rand_chacha::rand_core::RngCore;
+
 
 fn main() {
+    // Set up logging to both a file and the terminal
     CombinedLogger::init(vec![
         WriteLogger::new(LevelFilter::Warn, Config::default(), File::create(get_date_hour()).unwrap()),
         TermLogger::new(LevelFilter::Warn, Config::default(), TerminalMode::Mixed, ColorChoice::Auto)
     ]).unwrap();
+
+    let start = Instant::now(); // start the timer
+
+    // Process command line arguments and get the appropriate number of threads and wallets to generate
     let params = check_args(get_args());
+
+    // Set up a channel for sending messages between threads
     let (tx, rx) = channel();
 
-
+    // Spawn multiple threads
     for _ in 0..params.get("t").unwrap().parse::<u32>().unwrap() {
         let tx_ = tx.clone();
         let params_ = params.clone();
         let _ = spawn(move || thread_function(params_, tx_));
     }
 
-    //wait for wallets in channel and log them, then stop when reaching the limit
+    // Wait for wallets to be received on the channel and log them, then stop when reaching the limit
     for _ in 0..params.get("n").unwrap().parse::<u32>().unwrap() {
         let wallet: HashMap<String, String> = rx.recv().unwrap();
-        log::warn!("----------- new address found -----------\nAddress:     0x{}\nPrivate key: 0x{}\n\n", wallet.get("address").unwrap(), wallet.get("private_key").unwrap());
+        log::warn!(
+            "----------- new address found -----------\nAddress:     0x{}\nPrivate key: 0x{}\n\n",
+            wallet.get("address").unwrap(),
+            wallet.get("private_key").unwrap()
+        );
     }
 
-
+    // Print the total number of wallets generated and the elapsed time
+    println!(
+        "{} wallets generated in {:?}",
+        params.get("n").unwrap(),
+        start.elapsed()
+    );
 }
 
-//get date and hour to use as log file name
+// Get the current date and hour in a specific format to use as the log file name
 fn get_date_hour() -> String {
     let now = chrono::Local::now();
     let date = now.format("%Y-%m-%d");
@@ -39,10 +64,7 @@ fn get_date_hour() -> String {
     format!("{}-{}.log", date, hour)
 }
 
-
-
-
-//get hashmap of command line args starting with "-"
+// Parse the command line arguments and return them as a HashMap
 fn get_args() -> HashMap<String, String> {
     let mut args: HashMap<String, String> = HashMap::new();
     for arg in env::args() {
@@ -54,12 +76,12 @@ fn get_args() -> HashMap<String, String> {
     args
 }
 
-//check if number of args is correct and if the args are valid
+// Check if the number of command line arguments is correct and if the arguments are valid
 fn check_args(args: HashMap<String, String>) -> HashMap<String, String> {
     let mut params: HashMap<String, String> = HashMap::new();
-    //check if args contains "t" or "threads"
+    // Check if args contains "t" or "threads"
     if !args.contains_key("t") && !args.contains_key("threads") {
-        params.insert("t".to_string(), "1".to_string());
+        params.insert("t".to_string(), "1".to_string()); // default to 1 thread
     } else {
         if args.contains_key("t") {
             params.insert("t".to_string(), args.get("t").unwrap().to_string());
@@ -67,9 +89,9 @@ fn check_args(args: HashMap<String, String>) -> HashMap<String, String> {
             params.insert("t".to_string(), args.get("threads").unwrap().to_string());
         }
     }
-    //check if args contains "n" or "number"
+    // Check if args contains "n" or "number"
     if !args.contains_key("n") && !args.contains_key("number") {
-        params.insert("n".to_string(), "1".to_string());
+        params.insert("n".to_string(), "1".to_string()); // default to generating 1 wallet
     } else {
         if args.contains_key("n") {
             params.insert("n".to_string(), args.get("n").unwrap().to_string());
@@ -99,18 +121,21 @@ fn check_args(args: HashMap<String, String>) -> HashMap<String, String> {
 }
 
 //generate a random local wallet
-fn generate_wallet() -> HashMap<String, String> {
-    let random_bytes = rand::thread_rng().gen::<[u8; 32]>();
-
-    let private_key = SecretKey::parse(&random_bytes).unwrap();
-    let public_key = PublicKey::from_secret_key(&private_key);
-
+fn generate_wallet() -> HashMap<String, String> {    
+    let mut rng = ChaChaRng::from_entropy();
+    let mut bytes = [0u8; 32];
+    rng.fill_bytes(&mut bytes);
+    // Generate a secret key
+    let secret_key = SecretKey::parse(&bytes).unwrap();
+    // Generate a public key from the secret key
+    let public_key = PublicKey::from_secret_key(&secret_key);
+    // Hash the public key to get the Ethereum address
     let public_key_hash = Keccak256::digest(&public_key.serialize()[1..]);
     let address = hex::encode(&public_key_hash[12..]);
 
     let mut wallet: HashMap<String, String> = HashMap::new();
     wallet.insert("address".to_string(), address.to_string());
-    wallet.insert("private_key".to_string(), hex::encode(&private_key.serialize()).to_string());
+    wallet.insert("private_key".to_string(), hex::encode(&secret_key.serialize()).to_string());
     wallet
 }
 
@@ -120,22 +145,29 @@ fn thread_function(params: HashMap<String, String>, tx: Sender<HashMap<String, S
     loop {
         let wallet = generate_wallet();
         let address = wallet.get("address").unwrap();
-
-        if params.contains_key("p") {
-            if !address.starts_with(params.get("p").unwrap()) {
-                continue;
-            }
-        }
-        if params.contains_key("s") {
-            if !address.ends_with(params.get("s").unwrap()) {
-                continue;
-            }
-        }
-        if params.contains_key("a") {
-            if !address.contains(params.get("a").unwrap()) {
-                continue;
-            }
+        if !check_address(address, params.clone()) {
+            continue;
         }
         tx.send(wallet).unwrap();
     }
+}
+
+//check if address complies with params
+fn check_address(address: &str, params: HashMap<String, String>) -> bool {
+    if params.contains_key("p") {
+        if !address.starts_with(params.get("p").unwrap()) {
+            return false;
+        }
+    }
+    if params.contains_key("s") {
+        if !address.ends_with(params.get("s").unwrap()) {
+            return false;
+        }
+    }
+    if params.contains_key("a") {
+        if !address.contains(params.get("a").unwrap()) {
+            return false;
+        }
+    }
+    true
 }
