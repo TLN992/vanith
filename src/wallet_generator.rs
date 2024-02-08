fa58ff533d67858bd1744ae891882793c6cb72f9
992Fast <88493851+992Fast@users.noreply.github.com>
2024-02-08 17:28:29 +0100

    fixed issue

--- src/wallet_generator.rs
+++ src/wallet_generator.rs
@@ -1,39 +1,37 @@
-use rand::{SeedableRng, RngCore};
-use rand_isaac::IsaacRng; // used for generating random numbers
+use std::sync::mpsc::Sender;
+// used for generating hashes using the Keccak-256 algorithm
+use std::sync::{Arc, Mutex};
 use sha3::{Digest, Keccak256}; // used for generating hashes using the Keccak-256 algorithm
-use std::{
-    sync::{Arc, Mutex},
-};
-use std::sync::mpsc::Sender; // used for sending messages between threads
-use secp256k1::{Secp256k1, SecretKey, PublicKey, Signing};
+use rand_chacha::rand_core::SeedableRng;
+use rand_chacha::rand_core::RngCore;
+use rand_chacha::{ChaChaRng, ChaCha20Rng}; // used for generating random numbers
+use libsecp256k1::{PublicKey, SecretKey}; 
 
 use crate::args::Parameters;
 
 
 //generate random wallet
-fn generate_wallet<C: Signing>(rng: &mut IsaacRng, secp: &Secp256k1<C>) -> (String, String)  {
-    // Generate a secret key
-    // Pre-allocate memory for the bytes array
-    let mut bytes = vec![0u8; 32];
+fn generate_wallet(rng: &mut ChaCha20Rng) -> (String, String)  {    
+    let mut bytes = [0u8; 32];
     rng.fill_bytes(&mut bytes);
-    let secret_key = SecretKey::from_slice(&bytes).unwrap();
+    
+    let secret_key = SecretKey::parse(&bytes).unwrap();
 
     // Generate a public key from the secret key
-    let public_key = PublicKey::from_secret_key(&secp, &secret_key);
+    let public_key = PublicKey::from_secret_key(&secret_key);
 
-    // Hash the public key to get the Ethereum address
+    // Hash the public key to get the Ethereum address 
     let public_key_hash = Keccak256::digest(&public_key.serialize()[1..]);
-    (hex::encode(&public_key_hash[12..]).to_string(), hex::encode(&secret_key.secret_bytes()).to_string())
+    let address = hex::encode(&public_key_hash[12..]);
+    (address.to_string(), hex::encode(&secret_key.serialize()).to_string())
 }
 
 
 //thread function to generate wallets, check if they comply with params and send it back through channel
 pub fn thread_function(params: Parameters, tx: Sender<(String, String)>, flag: Arc<Mutex<bool>>) {
-    let mut rng = IsaacRng::from_entropy();
-    // Create a Secp256k1 context object
-    let secp = Secp256k1::new();
+    let mut rng = ChaChaRng::from_entropy();
     loop {
-        let wallet = generate_wallet(&mut rng, &secp);
+        let wallet = generate_wallet(&mut rng);
         if !check_address(&wallet.0, &params) {
             continue;
         }
